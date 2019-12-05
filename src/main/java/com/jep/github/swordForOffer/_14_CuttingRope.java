af0be0690ea1ac2fd718f2c6c7a774f529b05546
Enping <enping.jep@alipay.com>
2019-12-05 21:07:39 +0800

    commit

---
+++ src/main/java/com/jep/github/swordForOffer/_14_CuttingRope.java
@@ -0,0 +1,21 @@
+package com.jep.github.swordForOffer;
+
+/*
+ * @author: enping.jep
+ * @date: 2019/12/5
+ * @create 2019-12-05 8:07 PM
+ */
+
+//14：剪绳子
+// 题目：给你一根长度为n的绳子，请把绳子剪成m段（m、n都是整数，n>1并且m≥1）。
+// 每段的绳子的长度记为k[0]、k[1]、……、k[m]。k[0]*k[1]*…*k[m]可能的最大乘
+// 积是多少？例如当绳子的长度是8时，我们把它剪成长度分别为2、3、3的三段，此
+// 时得到最大的乘积18。
+
+public class _14_CuttingRope {
+
+
+  public static void main(String args[]){
+
+  }
+}
