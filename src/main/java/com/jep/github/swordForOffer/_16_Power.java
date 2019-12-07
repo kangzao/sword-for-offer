aa7757c38171c6531cb631fca7438d72fb779dd1
enping <58761173@qq.com>
2019-12-07 12:05:05 +0800

    数值的整数次方

--- src/main/java/com/jep/github/swordForOffer/_16_Power.java
+++ src/main/java/com/jep/github/swordForOffer/_16_Power.java
@@ -7,7 +7,7 @@ package com.jep.github.swordForOffer;
  */
 // 16：数值的整数次方
 // 题目：实现函数double Power(double base, int exponent)，求base的exponent次方。不得使用库函数，同时不需要考虑大数问题。
-public class Power {
+public class _16_Power {
 
 
     public static double power(double base, int exponent) {
