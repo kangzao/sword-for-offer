15d102df2cc9e5868e35c372bd6e04dde4edfe6d
enping <58761173@qq.com>
2020-02-25 20:46:03 +0800

    025 hard

---
+++ src/main/java/com/jep/github/leetcode/_025_ReverseKGroup.java
@@ -0,0 +1,99 @@
+package com.jep.github.leetcode;
+
+import com.jep.github.swordForOffer.ListNode;
+
+/**
+ * author jiangenping
+ * 2020/2/22 下午4:04
+ * <p>
+ * Given a linked list, reverse the nodes of a linked list k at a time and return its modified list.
+ * <p>
+ * k is a positive integer and is less than or equal to the length of the linked list.
+ * If the number of nodes is not a multiple of k then left-out nodes in the end should remain as it is.
+ * <p>
+ * Example:
+ * <p>
+ * Given this linked list: 1->2->3->4->5
+ * <p>
+ * For k = 2, you should return: 2->1->4->3->5
+ * <p>
+ * For k = 3, you should return: 3->2->1->4->5
+ * <p>
+ * Note:
+ * <p>
+ * Only constant extra memory is allowed.
+ * You may not alter the values in the list's nodes, only nodes itself may be changed.
+ */
+
+public class _025_ReverseKGroup {
+
+    /**
+     * Reverse a link list between pre and next exclusively
+     * an example:
+     * a linked list:
+     * 0->1->2->3->4->5->6
+     * |           |
+     * pre        next
+     * after call pre = reverse(pre, next)
+     * <p>
+     * 0->3->2->1->4->5->6
+     * |  |
+     * pre next
+     *
+     * @param begin 开区间，左侧
+     * @param end   开区间，右侧
+     * @return the reversed list's last node, which is the precedence of parameter next
+     */
+    private static ListNode reverse(ListNode begin, ListNode end) {
+        ListNode first = begin.next;
+        ListNode prev = begin;
+        ListNode cur = first;
+
+        while (cur != end) {
+            ListNode next = cur.next;
+            cur.next = prev;
+            prev = cur;
+            cur = next;
+        }
+        begin.next = prev;
+        first.next = cur;
+        return first;
+    }
+
+    public static ListNode reverseKGroup(ListNode head, int k) {
+        ListNode begin;
+        if (head == null || head.next == null || k == 1)
+            return head;
+        ListNode dummyhead = new ListNode(-1);
+        dummyhead.next = head;
+        begin = dummyhead;
+        int i = 0;
+        while (head != null) {
+            i++;
+            if (i % k == 0) {
+                begin = reverse(begin, head.next);
+                head = begin.next;
+            } else {
+                head = head.next;
+            }
+        }
+        return dummyhead.next;
+    }
+
+    public static void main(String[] args) {
+        ListNode node1 = new ListNode(1);
+        ListNode node2 = new ListNode(2);
+        ListNode node3 = new ListNode(3);
+        ListNode node4 = new ListNode(4);
+        ListNode node5 = new ListNode(5);
+        ListNode node6 = new ListNode(6);
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        node4.next = node5;
+        node5.next = node6;
+        reverse(node1, node6).printNode();
+        reverseKGroup(node1, 2).printNode();
+
+    }
+}
