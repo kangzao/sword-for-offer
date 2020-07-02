432a1f2ee79fff2392cb540a8020c4bd4aa2fc51
Enping <enping.jep@alipay.com>
2020-07-03 07:54:11 +0800

    88

---
+++ src/main/java/com/jep/github/leetcode/_88_Merge.java
@@ -0,0 +1,47 @@
+package com.jep.github.leetcode;
+
+import com.jep.github.swordForOffer.Util;
+
+/*
+ * @author: enping.jep
+ * @create: 2020-07-01 3:30 PM
+ *
+ * nums1 = [1,2,3,0,0,0], m = 3
+   nums2 = [2,5,6],       n = 3
+
+   输出: [1,2,2,3,5,6]
+ */
+public class _88_Merge {
+
+  public static void merge(int[] A, int m, int[] B, int n) {
+    int p1 = m - 1, p2 = n - 1, p = m + n - 1;
+    ;
+    while (p1 >= 0 && p2 >= 0) {
+      if (A[p1] > B[p2]) {
+        A[p--] = A[p1--];
+      } else {
+        A[p--] = B[p2--];
+      }
+    }
+    while (p2 >= 0) {
+      A[p--] = B[p2--];
+    }
+
+
+  }
+
+  public static void main(String args[]) {
+    int[] nums5 = {1, 2, 3, 0, 0, 0}, nums6 = {2, 5, 6};
+
+//        mergeSortedArray(nums1, 3, nums2, 4);
+//        Util.printArray(nums1);
+////        Util.printArray(nums2);
+//        mergeSortedArray(nums3, 0, nums4, 1);
+//        Util.printArray(nums3);
+    merge(nums5, 3, nums6, 3);
+
+    Util.printArray(nums5);
+    merge(nums5, 3, nums6, 3);
+  }
+
+}
