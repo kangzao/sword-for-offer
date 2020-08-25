719a00b9d5d5f5698aad929247fb4533b563f9bc
enping <58761173@qq.com>
2020-08-25 22:43:02 +0800

    堆排序

--- src/main/java/com/jep/github/algorithm/HeapSort.java
+++ src/main/java/com/jep/github/algorithm/HeapSort.java
@@ -1,63 +1,64 @@
 package com.jep.github.algorithm;
 
 import com.jep.github.swordForOffer.Util;
+
 import java.util.ArrayDeque;
 import java.util.Arrays;
 
 public class HeapSort {
 
+    /**
+     * @param array  数组
+     * @param length 数组长度
+     * @param index  元素位置
+     *               从顶部向下堆化(前提是，左右子树都已经是堆了)
+     */
+    public static void heapify(int[] array, int length, int index) {
+        if (index >= length) {
+            return;
+        }
+        int left = 2 * index + 1;
+        int right = 2 * index + 2;
 
-  /**
-   * @param array 数组
-   * @param length 数组长度
-   * @param index 元素位置
-   */
-  public static void heapify(int[] array, int length, int index) {
-    if (index >= length) {
-      return;
-    }
-    int c1 = 2 * index + 1;
-    int c2 = 2 * index + 2;
-    int maxIndex = index;
-    if (c1 < length && array[c1] > array[maxIndex]) {
-      maxIndex = c1;
-    }
-    if (c2 < length && array[c2] > array[maxIndex]) {
-      maxIndex = c2;
-    }
-
-    if (maxIndex != index) {
-      Util.swap(array, maxIndex, index);
-      heapify(array, length, maxIndex);
-    }
-//    System.out.println(Arrays.toString(array));
-  }
-
-
-  public static void buildMaxHeap(int[] array) {
-    int start = (array.length - 1) / 2;
-    for (int i = start; i >= 0; i--) {
-      System.out.println(i);
-      heapify(array, array.length, i);
-    }
-  }
-
-  public static void sort(int[] array) {
-    buildMaxHeap(array);//堆构造
-    int index = array.length - 1;
-    while (index >= 0) {
-      Util.swap(array, 0, index);
-      heapify(array, index--, 0);
+        int maxIndex = index;
+        if (left < length && array[left] > array[maxIndex]) {
+            maxIndex = left;
+        }
+        if (right < length && array[right] > array[maxIndex]) {
+            maxIndex = right;
+        }
+        if (maxIndex != index) {
+            Util.swap(array, maxIndex, index);
+            heapify(array, length, maxIndex);
+        }
     }
 
-  }
+    public static void buildHeap(int[] array, int n) {
+        int lastNode = n - 1;
+        //parent = (i - 1)/2
+        int parent = (lastNode - 1) / 2;
+        for (int i = parent; i >= 0; i--) {
+            //非叶子节点分别进行heapify
+            heapify(array, n, i);
+        }
+    }
 
 
-  public static void main(String[] args) {
-    int[] arr = {20, 50, 20, 40, 70, 10, 80, 30, 60};
-//    heapify(arr, 9, 0);
-//    System.out.println(Arrays.toString(arr));//[50, 70, 20, 40, 20, 10, 80, 30, 60]
-    sort(arr);
-    System.out.println(Arrays.toString(arr));//[80, 70, 20, 60, 50, 10, 20, 30, 40]
-  }
+    public static void sort(int[] array, int n) {
+        buildHeap(array, n);
+        for (int i = n - 1; i >= 0; i--) {
+            Util.swap(array, i, 0);
+            //i代表当前节点的数量
+            heapify(array, i, 0);
+        }
+
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {20, 50, 20, 40, 70, 10, 80, 30, 60};
+        heapify(arr, 9, 0);
+        System.out.println(Arrays.toString(arr));//[50, 70, 20, 40, 20, 10, 80, 30, 60]
+        sort(arr, 9);
+        System.out.println(Arrays.toString(arr));//[80, 70, 20, 60, 50, 10, 20, 30, 40]
+    }
 }
