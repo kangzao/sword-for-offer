9ec3c400a6fee812bd561a8b94ad4cbfa2a2d929
enping <58761173@qq.com>
2020-09-16 08:31:56 +0800

    44

--- src/main/java/com/jep/github/swordForOffer/_44_FindNthDigit.java
+++ src/main/java/com/jep/github/swordForOffer/_44_FindNthDigit.java
@@ -7,12 +7,22 @@ package com.jep.github.swordForOffer;
 public class _44_FindNthDigit {
 
 
-
     public int findNthDigit(int n) {
+
+        long index = n;
+        //已经遍历的数量
+        long count = 0;
+        long base = 1;
+        //先求出占位的区间  如果在1-9  count数量是9*1  1-99 count的数量是9+9*10 1-999  count的数量是9+9*10+9*100
+        while (count + base * 9 < index) {
+            count += base * 9;
+            base *= 10;
+        }
+        //根据count的值
 
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
     }
 }
