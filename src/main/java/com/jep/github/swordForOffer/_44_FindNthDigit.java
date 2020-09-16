1b6516638df7cf7d2b399822d25ec70536c93394
enping <58761173@qq.com>
2020-09-16 08:12:22 +0800

    44

---
+++ src/main/java/com/jep/github/swordForOffer/_44_FindNthDigit.java
@@ -0,0 +1,18 @@
+package com.jep.github.swordForOffer;
+
+/**
+ * @author jiangenping
+ * @date 2020/9/15 下午8:45
+ */
+public class _44_FindNthDigit {
+
+
+
+    public int findNthDigit(int n) {
+
+    }
+
+    public static void main(String[] args){
+
+    }
+}
