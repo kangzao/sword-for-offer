9e608aa3f897ff547fd5e299469883603ba52f63
Enping <enping.jep@alipay.com>
2020-09-16 16:34:43 +0800

    44

--- src/main/java/com/jep/github/swordForOffer/_44_FindNthDigit.java
+++ src/main/java/com/jep/github/swordForOffer/_44_FindNthDigit.java
@@ -7,22 +7,44 @@ package com.jep.github.swordForOffer;
 public class _44_FindNthDigit {
 
 
-    public int findNthDigit(int n) {
+  public static int findNthDigit(int n) {
 
-        long index = n;
-        //已经遍历的数量
-        long count = 0;
-        long base = 1;
-        //先求出占位的区间  如果在1-9  count数量是9*1  1-99 count的数量是9+9*10 1-999  count的数量是9+9*10+9*100
-        while (count + base * 9 < index) {
-            count += base * 9;
-            base *= 10;
-        }
-        //根据count的值
+    char res = '0';
 
+    long index = n;
+    //左边界
+    long border = 0;
+    //base表示数字的位数，比如：10的位数是2 1的位数是1
+    long base = 1;
+    //1-9 有9个数字，10-99 有90个数字
+    long digitNum = 9;
+    //先求出范围，比如：0123456789101112 求第14位，首先确定第14位的数字是在10-99这个区间内
+    //递推公式 border = border + base*digitalNum  1-9这个区间， border是9, 循环执行完border是9
+    while (border + base * digitNum < index) {
+      border += base * digitNum;
+      base += 1;
+      digitNum *= 10;
     }
-
-    public static void main(String[] args) {
-
+    //border=9则确定14在10-99这个区间内，14-9 = 5, base = 2,5/2 = 2,则该数字在10+2 = 12这个数字中(走过两个数字),5%2 = 1说明在12这个数字中的第一个位置上
+    long a = (index - border) / base;
+    long b = (index - border) % base;
+    //该位置所在的数字是多少
+    long c;
+    if (b != 0) {
+      //没有整除，则走过a个数字，落在12这个位置
+      c = (long) (Math.pow(10, base - 1) + a);
+      res = String.valueOf(c).charAt((int) b - 1);
+    } else {
+      //如果恰好整除，走过两个数字之后前移一位
+      c = (long) (Math.pow(10, base - 1) + a - 1);
+      //这个数字的最后一位
+      res = String.valueOf(c).charAt((int) base - 1);
     }
+    return Integer.parseInt(String.valueOf(res));
+
+  }
+
+  public static void main(String[] args) {
+    System.out.println(findNthDigit(3));
+  }
 }
