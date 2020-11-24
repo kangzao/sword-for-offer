1d311d4d1645d5ef965ff9a044ad2a7a0ae19b7e
Enping <enping.jep@alipay.com>
2020-11-24 10:45:37 +0800

    206

--- src/main/java/com/jep/github/leetcode/_206_ReverseLinkedList.java
+++ src/main/java/com/jep/github/leetcode/_206_ReverseLinkedList.java
@@ -14,35 +14,45 @@ import com.jep.github.swordForOffer.ListNode;
  */
 public class _206_ReverseLinkedList {
 
-    public static ListNode reverseList(ListNode head) {
-        ListNode newHead = null;
-        while (head != null) {
-            ListNode next = head.next;
-            head.next = newHead;
-            newHead = head;
-            head = next;
-        }
-        return newHead;
+  public static ListNode reverseList(ListNode head) {
+    ListNode newHead = null;
+    while (head != null) {
+      ListNode next = head.next;
+      head.next = newHead;
+      newHead = head;
+      head = next;
     }
+    return newHead;
+  }
 
-
-    public static void main(String args[]) {
-        ListNode node1 = new ListNode(1);
-        ListNode node2 = new ListNode(2);
-        ListNode node3 = new ListNode(3);
-        ListNode node4 = new ListNode(4);
-        ListNode node5 = new ListNode(5);
-        ListNode node6 = new ListNode(6);
-        node1.next = node2;
-        node2.next = node3;
-        node3.next = node4;
-        node4.next = node5;
-        node5.next = node6;
-        node1.printNode();
-        ListNode newNode = reverseList(node1);
-        newNode.printNode();
-
-
+  public static ListNode reverseList_recursion(ListNode head) {
+    if (head == null || head.next == null) {
+      return head;
     }
+    ListNode last = reverseList_recursion(head.next);
+    head.next.next = head;
+    head.next = null;
+    return last;
+  }
+
+
+  public static void main(String args[]) {
+    ListNode node1 = new ListNode(1);
+    ListNode node2 = new ListNode(2);
+    ListNode node3 = new ListNode(3);
+    ListNode node4 = new ListNode(4);
+    ListNode node5 = new ListNode(5);
+    ListNode node6 = new ListNode(6);
+    node1.next = node2;
+    node2.next = node3;
+    node3.next = node4;
+    node4.next = node5;
+    node5.next = node6;
+    node1.printNode();
+    ListNode newNode = reverseList_recursion(node1);
+    newNode.printNode();
+
+
+  }
 
 }
