beefbbd7a4a9ce6279e9001d09b0255822ffee96
Enping <enping.jep@alipay.com>
2020-12-01 16:08:30 +0800

    25

--- src/main/java/com/jep/github/leetcode/_025_ReverseKGroup.java
+++ src/main/java/com/jep/github/leetcode/_025_ReverseKGroup.java
@@ -3,13 +3,13 @@ package com.jep.github.leetcode;
 import com.jep.github.swordForOffer.ListNode;
 
 /**
- * author jiangenping
- * 2020/2/22 下午4:04
+ * author jiangenping 2020/2/22 下午4:04
  * <p>
- * Given a linked list, reverse the nodes of a linked list k at a time and return its modified list.
+ * Given a linked list, reverse the nodes of a linked list k at a time and return its modified
+ * list.
  * <p>
- * k is a positive integer and is less than or equal to the length of the linked list.
- * If the number of nodes is not a multiple of k then left-out nodes in the end should remain as it is.
+ * k is a positive integer and is less than or equal to the length of the linked list. If the number
+ * of nodes is not a multiple of k then left-out nodes in the end should remain as it is.
  * <p>
  * Example:
  * <p>
@@ -21,79 +21,84 @@ import com.jep.github.swordForOffer.ListNode;
  * <p>
  * Note:
  * <p>
- * Only constant extra memory is allowed.
- * You may not alter the values in the list's nodes, only nodes itself may be changed.
+ * Only constant extra memory is allowed. You may not alter the values in the list's nodes, only
+ * nodes itself may be changed.
  */
 
 public class _025_ReverseKGroup {
 
-    /**
-     * Reverse a link list between pre and next exclusively
-     * an example:
-     * a linked list:
-     * 0->1->2->3->4->5->6
-     * |           |
-     * pre        next
-     * after call pre = reverse(pre, next)
-     * <p>
-     * 0->3->2->1->4->5->6
-     * |  |
-     * pre next
-     *
-     * @param begin 开区间，左侧
-     * @param end   开区间，右侧
-     * @return the reversed list's last node, which is the precedence of parameter next
-     */
-    private static ListNode reverse(ListNode begin, ListNode end) {
-        ListNode first = begin.next;
-        ListNode prev = begin;
-        ListNode cur = first;
+  /**
+   * 反转链表，返回新的头和尾
+   */
+  private static ListNode[] reverse(ListNode begin, ListNode end) {
+    ListNode reverseTail = begin;
+    ListNode reverseHead = null;
+    while (reverseHead != end) {
+      ListNode next = begin.next;
+      begin.next = reverseHead;
+      reverseHead = begin;
+      begin = next;
+    }
+    return new ListNode[]{reverseHead, reverseTail};
+  }
 
-        while (cur != end) {
-            ListNode next = cur.next;
-            cur.next = prev;
-            prev = cur;
-            cur = next;
+  public static ListNode reverseKGroup(ListNode head, int k) {
+    ListNode dummy = new ListNode(-1);
+    dummy.next = head;
+
+    //被翻转子链表前的节点
+    ListNode pre = dummy;
+
+    while (head != null) {
+      //子链表的最后一个节点
+      ListNode end = pre;
+      //走k步后停下来，如果长度不够K，则返回头结点
+      for (int i = 0; i < k; i++) {
+        end = end.next;
+        if (end == null) {
+          return dummy.next;
         }
-        begin.next = prev;
-        first.next = cur;
-        return first;
-    }
+      }
 
-    public static ListNode reverseKGroup(ListNode head, int k) {
-        ListNode begin;
-        if (head == null || head.next == null || k == 1)
-            return head;
-        ListNode dummyhead = new ListNode(-1);
-        dummyhead.next = head;
-        begin = dummyhead;
-        int i = 0;
-        while (head != null) {
-            i++;
-            if (i % k == 0) {
-                begin = reverse(begin, head.next);
-                head = begin.next;
-            } else {
-                head = head.next;
-            }
-        }
-        return dummyhead.next;
-    }
+      //子链表的下一个节点
+      ListNode next = end.next;
+      //翻转子链表
+      ListNode[] listNodes = reverse(head, end);
+      //翻转后的子链表头部
+      head = listNodes[0];
+      //翻转后的子链表尾部
+      end = listNodes[1];
 
-    public static void main(String[] args) {
-        ListNode node1 = new ListNode(1);
-        ListNode node2 = new ListNode(2);
-        ListNode node3 = new ListNode(3);
-        ListNode node4 = new ListNode(4);
-        ListNode node5 = new ListNode(5);
-        ListNode node6 = new ListNode(6);
-        node1.next = node2;
-        node2.next = node3;
-        node3.next = node4;
-        node4.next = node5;
-        node5.next = node6;
-        reverse(node1, node6).printNode();
-        reverseKGroup(node1, 2).printNode();
+      //连接前半部分
+      pre.next = head;
+      //连接后半部分
+      end.next = next;
+
+      pre = end;
+      head = end.next;
+
 
     }
+
+    return dummy.next;
+  }
+
+
+  public static void main(String[] args) {
+    ListNode node1 = new ListNode(1);
+    ListNode node2 = new ListNode(2);
+    ListNode node3 = new ListNode(3);
+    ListNode node4 = new ListNode(4);
+    ListNode node5 = new ListNode(5);
+    ListNode node6 = new ListNode(6);
+    node1.next = node2;
+    node2.next = node3;
+    node3.next = node4;
+    node4.next = node5;
+    node5.next = node6;
+//    node1.printNode();
+//    reverse(node1, node6);
+    reverseKGroup(node1, 2).printNode();
+
+  }
 }
