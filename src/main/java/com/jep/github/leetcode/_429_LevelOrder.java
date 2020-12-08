ac35373e0af9bdf32f473bbce7c1ad8acf946465
Enping <enping.jep@alipay.com>
2020-12-08 19:27:08 +0800

    429

---
+++ src/main/java/com/jep/github/leetcode/_429_LevelOrder.java
@@ -0,0 +1,35 @@
+package com.jep.github.leetcode;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/*
+ * @author: enping.jep
+ * @create: 2020-12-03 3:43 PM
+ */
+public class _429_LevelOrder {
+
+  public static List<List<Integer>> levelOrder(Node root) {
+    List<List<Integer>> result = new ArrayList<>();
+    if (root == null) {
+      return result;
+    }
+    Queue<Node> queue = new LinkedList<>();
+    queue.add(root);
+    while (!queue.isEmpty()) {
+      List<Integer> level = new ArrayList<>();
+      int size = queue.size();
+      for (int i = 0; i < size; i++) {
+        Node node = queue.poll();
+        level.add(node.val);
+        queue.addAll(node.children);
+      }
+      result.add(level);
+    }
+    return result;
+  }
+
+}
