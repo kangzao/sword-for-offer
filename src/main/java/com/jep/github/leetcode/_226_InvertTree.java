2d18cee85025bd12631c9345324c68af3a9336d4
Enping <enping.jep@alipay.com>
2020-12-08 19:56:07 +0800

    226

---
+++ src/main/java/com/jep/github/leetcode/_226_InvertTree.java
@@ -0,0 +1,32 @@
+package com.jep.github.leetcode;
+
+import apple.laf.JRSUIUtils.Tree;
+
+/*
+ * @author: enping.jep
+ * @create: 2020-12-08 7:42 PM
+ */
+public class _226_InvertTree {
+
+  public static TreeNode invertTree(TreeNode root) {
+    if (root == null) {
+      return root;
+    }
+    TreeNode tmp = root.left;
+    root.left = root.right;
+    root.right = tmp;
+
+    invertTree(root.left);
+    invertTree(root.right);
+
+    return root;
+  }
+
+  public static void main(String args[]) {
+    TreeNode root = new TreeNode(1);
+    root.left = new TreeNode(2);
+    root.right = new TreeNode(3);
+    System.out.println(invertTree(root));
+  }
+
+}
