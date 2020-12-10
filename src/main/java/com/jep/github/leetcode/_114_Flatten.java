8fe12ae92ba0f9e19d13b1a9422cfbb1e403ae3d
Enping <enping.jep@alipay.com>
2020-12-10 17:25:33 +0800

    114

---
+++ src/main/java/com/jep/github/leetcode/_114_Flatten.java
@@ -0,0 +1,52 @@
+package com.jep.github.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ * @author: enping.jep
+ * @create: 2020-12-10 3:50 PM
+ */
+public class _114_Flatten {
+
+  public static void flatten(TreeNode root) {
+    List<TreeNode> list = new ArrayList<>();
+    preorderTraversal(root, list);
+
+    for (int i = 1; i < list.size(); i++) {
+      TreeNode tmp;
+      tmp = list.get(i - 1);
+      tmp.right = list.get(i);
+      tmp.left = null;
+    }
+
+  }
+
+
+  //前序遍历
+  public static void preorderTraversal(TreeNode root, List<TreeNode> list) {
+    if (root == null) {
+      return;
+    }
+    list.add(root);
+    preorderTraversal(root.left, list);
+    preorderTraversal(root.right, list);
+
+  }
+
+  public static void main(String args[]) {
+
+    TreeNode treeNode3 = new TreeNode(15);
+    TreeNode treeNode4 = new TreeNode(7);
+    TreeNode treeNode1 = new TreeNode(9);
+    TreeNode treeNode2 = new TreeNode(20);
+    treeNode1.left = treeNode3;
+    treeNode2.right = treeNode4;
+    TreeNode root = new TreeNode(3, treeNode1, treeNode2);
+    flatten(root);
+    System.out.println(root);
+
+
+  }
+
+}
