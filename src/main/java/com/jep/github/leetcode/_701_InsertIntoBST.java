f37c52acab94e643bfe702e9ce6dd8a7e2dcd79b
enping <58761173@qq.com>
2021-01-04 21:49:03 +0800

    701

--- src/main/java/com/jep/github/leetcode/_701_InsertIntoBST.java
+++ src/main/java/com/jep/github/leetcode/_701_InsertIntoBST.java
@@ -13,23 +13,43 @@ package com.jep.github.leetcode;
 public class _701_InsertIntoBST {
 
 
-  public TreeNode insertIntoBST(TreeNode root, int val) {
-    if (root == null) {
-      return new TreeNode(val);
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        if (root == null) {
+            return new TreeNode(val);
+        }
+        //插入右子树
+        if (val > root.val) {
+            root.right = insertIntoBST(root.right, val);
+        }
+        //插入左子树
+        else if (val < root.val) {
+            root.left = insertIntoBST(root.left, val);
+        }
+        return root;
     }
-    //插入右子树
-    if (val > root.val) {
-      root.right = insertIntoBST(root.right, val);
-    }
-    //插入左子树
-    else if (val < root.val) {
-      root.left = insertIntoBST(root.left, val);
-    }
-    return root;
-  }
 
-  public static void main(String args[]) {
+    public static void main(String args[]) {
 
-  }
+
+        TreeNode root = new TreeNode(5);
+        TreeNode treeNode1 = new TreeNode(3);
+        TreeNode treeNode2 = new TreeNode(6);
+        TreeNode treeNode3 = new TreeNode(1);
+        TreeNode treeNode4 = new TreeNode(4);
+        TreeNode treeNode5 = new TreeNode(7);
+
+        root.left = treeNode1;
+        root.right = treeNode2;
+
+        treeNode1.left = treeNode3;
+        treeNode1.right = treeNode4;
+        treeNode2.right = treeNode5;
+
+        _701_InsertIntoBST instance = new _701_InsertIntoBST();
+
+        TreeNode treeNode = instance.insertIntoBST(root, 2);
+      System.out.println(treeNode);
+
+    }
 
 }
