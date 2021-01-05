bc79e827cc2a6d92f6ba60d198fbb3b18dbce73c
Enping <enping.jep@alipay.com>
2021-01-05 10:44:32 +0800

    700

--- src/main/java/com/jep/github/leetcode/_700_SearchBST.java
+++ src/main/java/com/jep/github/leetcode/_700_SearchBST.java
@@ -5,17 +5,16 @@ package com.jep.github.leetcode;
  * @date 2021/1/4 下午9:58
  **/
 public class _700_SearchBST {
-    public TreeNode searchBST(TreeNode root, int val) {
-        if (root == null) {
-            return null;
-        }
 
-        if (root.val == val) {
-            return root;
-        } else if (root.val > val) {
-            return searchBST(root.left, val);
-        } else {
-            return searchBST(root.right, val);
-        }
+  public TreeNode searchBST(TreeNode root, int val) {
+    if (root == null || root.val == val) {
+      return root;
     }
+
+    if (root.val > val) {
+      return searchBST(root.left, val);
+    } else {
+      return searchBST(root.right, val);
+    }
+  }
 }
