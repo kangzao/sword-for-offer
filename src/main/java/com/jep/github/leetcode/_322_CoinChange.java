13ac9073d90cd0f3dd6d5502395976cf2e92f43b
Enping <enping.jep@alipay.com>
2021-01-07 14:02:56 +0800

    322

--- src/main/java/com/jep/github/leetcode/_322_CoinChange.java
+++ src/main/java/com/jep/github/leetcode/_322_CoinChange.java
@@ -87,6 +87,7 @@ public class _322_CoinChange {
    */
   public int coinChange_dp_loop(int[] coins, int amount) {
     int[] dp = new int[amount + 1];
+    // 注意：因为要比较的是最小值，这个不可能的值就得赋值成为一个最大值
     Arrays.fill(dp, amount + 1);
     dp[0] = 0;
     for (int i = 1; i <= amount; i++) {
