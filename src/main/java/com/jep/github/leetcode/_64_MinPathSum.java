28dada5fb6624b1ef073ce2562c88833fa91b4ef
Enping <enping.jep@alipay.com>
2021-01-14 08:26:53 +0800

    64

--- src/main/java/com/jep/github/leetcode/_64_MinPathSum.java
+++ src/main/java/com/jep/github/leetcode/_64_MinPathSum.java
@@ -29,17 +29,54 @@ public class _64_MinPathSum {
     }
     for (int i = 1; i < row; i++) {
       for (int j = 1; j < col; j++) {
-        dp[i][j] = Math.min(dp[i - 1][j] + grid[i][j], dp[i][j - 1] + grid[i][j]);
+        dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
       }
     }
     return dp[row - 1][col - 1];
 
   }
 
+  public int minPathSum_rollArray(int[][] grid) {
+    int m = grid.length;
+    if (m == 0) {
+      return 0;
+    }
+    int n = grid[0].length;
+    if (n == 0) {
+      return 0;
+    }
+
+    int[][] dp = new int[2][n];
+    int old, now = 0;
+//        dp[i][j] = min{dp[i-1][j],dp[i][j-1]}+grid[i][j]
+    for (int i = 0; i < m; i++) {
+      old = now;
+      now = 1 - now;
+      for (int j = 0; j < n; j++) {
+        if (i == 0 && j == 0) {
+          dp[now][j] = grid[i][j];
+          continue;
+        }
+        int tmp = Integer.MAX_VALUE;
+        if (i != 0) {
+          tmp = Math.min(tmp, dp[old][j]);
+        }
+
+        if (j != 0) {
+          tmp = Math.min(tmp, dp[now][j - 1]);
+        }
+        dp[now][j] = tmp + grid[i][j];
+      }
+    }
+    return dp[now][n - 1];
+
+  }
+
+
   public static void main(String args[]) {
     _64_MinPathSum solution = new _64_MinPathSum();
     int[][] array = {{1, 3, 1}, {1, 5, 1}, {4, 2, 1}};
-    System.out.println(solution.minPathSum(array));
+    System.out.println(solution.minPathSum_rollArray(array));
   }
 
 
