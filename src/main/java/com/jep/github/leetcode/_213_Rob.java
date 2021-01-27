637119db4563edf2ba74cfc71417814f87bb2b7a
enping <58761173@qq.com>
2021-01-27 20:56:46 +0800

    213

--- src/main/java/com/jep/github/leetcode/_213_Rob.java
+++ src/main/java/com/jep/github/leetcode/_213_Rob.java
@@ -6,24 +6,31 @@ package com.jep.github.leetcode;
  */
 public class _213_Rob {
 
-  public int rob(int[] nums) {
-    if (nums == null || nums.length == 0) {
-      return 0;
-    }
-    int[] dp = new int[nums.length + 1];
-    dp[0] = 0;
-    dp[1] = nums[0];
-    //[2,7,9,3,1]
-    //dp[i]代表前N个元素经过选择后的最大值
-    for (int i = 2; i < nums.length; i++) {
-      dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i - 1]);
+    public int rob(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        if (nums.length == 1) {
+            return nums[0];
+        }
+        return Math.max(robHelper(nums, 1, nums.length - 1), robHelper(nums, 0, nums.length - 2));
+
     }
 
-    return dp[nums.length];
-  }
+    //
+    private int robHelper(int[] nums, int lo, int hi) {
+        int include = 0, exclude = 0;
+        for (int j = lo; j <= hi; j++) {
+            int i = include, e = exclude;
+            include = e + nums[j];
+            exclude = Math.max(e, i);
+        }
+        return Math.max(include, exclude);
+    }
 
-  public static void main(String args[]) {
 
-  }
+    public static void main(String args[]) {
+
+    }
 
 }
