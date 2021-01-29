0db368b0af55ac1b9e683fd2302778b2b859aa50
Enping <enping.jep@alipay.com>
2021-01-29 12:03:21 +0800

    122

--- src/main/java/com/jep/github/leetcode/_122_MaxProfit.java
+++ src/main/java/com/jep/github/leetcode/_122_MaxProfit.java
@@ -19,6 +19,18 @@ public class _122_MaxProfit {
     return profit;
   }
 
+  //分两种情况，一种是当天已经没有股票，一直是当天持有股票
+  //dp[i][0]表示前i天的当天没有持有股票 dp[i][1]表示前i天的当天持有股票
+  public static int maxProfit_dp(int[] prices) {
+    int[][] dp = new int[prices.length][2];
+    dp[0][0]
+    //当天没有持股，则前一天可以持股，也可以不持股：
+    //如果前一天持股，dp[i][0] = dp[i-1][1] + prices[i]  前一天不持股 dp[i][0] = dp[i-1][0]
+
+    //当天持股
+    //前一天持股  dp[i][1] = dp[i-1][1]  前一天不持股 dp[i][1] = dp[i-1][0] - prices[i]
+  }
+
   public static void main(String args[]) {
     int array[] = {7, 1, 5, 3, 6, 4};
     System.out.println(maxProfit(array));
