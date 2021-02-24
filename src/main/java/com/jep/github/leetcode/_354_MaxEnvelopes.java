0b7402e1ad84d9fab430646d846b3a7c49f94dfd
Enping <enping.jep@alipay.com>
2021-02-24 18:59:56 +0800

    354

---
+++ src/main/java/com/jep/github/leetcode/_354_MaxEnvelopes.java
@@ -0,0 +1,14 @@
+package com.jep.github.leetcode;
+
+/*
+ * @author: enping.jep
+ * @create: 2021-02-24 4:44 下午
+ */
+public class _354_MaxEnvelopes {
+
+  public static void main(String args[]) {
+    int[][] envelopes = {{5, 4},[6, 4],[6, 7],[2, 3]};
+  }
+
+
+}
