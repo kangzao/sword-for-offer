6241316866fcc9548784db37b3d274614414001d
Enping <enping.jep@alipay.com>
2021-02-24 16:36:10 +0800

    300

--- src/main/java/com/jep/github/leetcode/_300_LengthOfLIS.java
+++ src/main/java/com/jep/github/leetcode/_300_LengthOfLIS.java
@@ -12,21 +12,24 @@ import java.util.Arrays;
 public class _300_LengthOfLIS {
 
   public int lengthOfLIS(int[] nums) {
+    //状态转移公式 dp数组保存每步子问题的最优解，dp[i]代表第i个元素的最长上升子序列的长度
+    //求解dp[i]时，向前遍历找到比i元素小的j,
+    //dp[i] = Math.max(dp[i], dp[j]+1);
     if (nums.length == 0) {
       return 0;
     }
     int[] dp = new int[nums.length];
-    int res = 0;
     Arrays.fill(dp, 1);
+    int result = 0;
     for (int i = 0; i < nums.length; i++) {
       for (int j = 0; j < i; j++) {
         if (nums[j] < nums[i]) {
           dp[i] = Math.max(dp[i], dp[j] + 1);
         }
       }
-      res = Math.max(res, dp[i]);
+      result = Math.max(result, dp[i]);
     }
-    return res;
+    return result;
   }
 
 
