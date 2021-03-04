658852a0629ba1e5def6f6f702da91fa18ea1787
Enping <enping.jep@alipay.com>
2021-03-04 15:50:36 +0800

    279

--- src/main/java/com/jep/github/leetcode/_279_NumSquares.java
+++ src/main/java/com/jep/github/leetcode/_279_NumSquares.java
@@ -6,4 +6,17 @@ package com.jep.github.leetcode;
  */
 public class _279_NumSquares {
 
+  public int numSquares(int n) {
+    //状态转移方程 dp[i] = Math.min(dp[i],dp[i-j*j]+1);
+    //j*j表示最后一个完全平方数
+    int[] dp = new int[n + 1];
+    for (int i = 0; i < dp.length; i++) {
+      dp[i] = i;//初始化，求最小值，初始化成最坏情况：i由i个完全平方数1组成
+      for (int j = 1; i - j * j >= 0; j++) {
+        dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
+      }
+    }
+    return dp[n];
+  }
+
 }
