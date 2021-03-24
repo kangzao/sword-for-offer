e7aadeba12a880317fe91ea65d933a2b7dc4ad2d
Enping <enping.jep@alipay.com>
2021-03-24 14:46:16 +0800

    410

---
+++ src/main/java/com/jep/github/leetcode/_410_SplitArray.java
@@ -0,0 +1,13 @@
+package com.jep.github.leetcode;
+
+/*
+ * @author: enping.jep
+ * @create: 2021-03-24 2:06 下午
+ * https://leetcode-cn.com/problems/split-array-largest-sum/solution/er-fen-cha-zhao-by-liweiwei1419-4/
+ */
+public class _410_SplitArray {
+
+  public int splitArray(int[] nums, int m) {
+    return 0;
+  }
+}
