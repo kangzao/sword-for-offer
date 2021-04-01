f59df4bae0d2375abc8f0fddb6b6ab90e5479b9e
Enping <enping.jep@alipay.com>
2021-04-01 10:09:07 +0800

    53

--- src/main/java/com/jep/github/leetcode/_053_MaxSubArray.java
+++ src/main/java/com/jep/github/leetcode/_053_MaxSubArray.java
@@ -24,6 +24,31 @@ public class _053_MaxSubArray {
     return result;
   }
 
+
+  // 在每一个扫描点计算以该点数值为结束点的子数列的最大和（正数和）。
+  // 该子数列由两部分组成：以前一个位置为结束点的最大子数列、该位置的数值。
+  // 因为该算法用到了“最佳子结构”（以每个位置为终点的最大子数列都是基于其前一位置的最大子数列计算得出,
+  // 该算法可看成动态规划的一个例子。
+  // 状态转移方程：sum[i] = max{sum[i-1]+a[i],a[i]}
+  // 其中(sum[i]记录以a[i]为子序列末端的最大序子列连续和)
+  public int maxSubArray_dp(int[] nums) {
+    int n = nums.length;
+    if (n == 1) {
+      return nums[0];
+    }
+    int[] dp = new int[n];
+    dp[0] = nums[0];
+    int max = nums[0];
+    for (int i = 1; i < n; i++) {
+      //子数组的和的最大值，dp[i-1]子数组如果小于0，则nums[i]大，于是从i开始新的子数组
+      //dp[i-1]子数组大于0，则dp[i - 1]+nums[i]比nums[i]大，nums[i]加入子数组中
+      //以每个位置为终点的最大子数列 都是基于其前一位置的最大子数列计算得出
+      dp[i] = Math.max(nums[i], dp[i - 1] + nums[i]);
+      max = Math.max(dp[i], max);
+    }
+    return max;
+  }
+
   public static void main(String args[]) {
     int[] nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
     int[] nums1 = {1};
