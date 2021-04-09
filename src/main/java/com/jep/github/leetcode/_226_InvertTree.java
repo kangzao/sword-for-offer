e5f00c8f21659ff3259fc123c5397b30cde93385
Enping <enping.jep@alipay.com>
2021-04-09 12:39:22 +0800

    226

--- src/main/java/com/jep/github/leetcode/_226_InvertTree.java
+++ src/main/java/com/jep/github/leetcode/_226_InvertTree.java
@@ -14,10 +14,8 @@ public class _226_InvertTree {
     TreeNode tmp = root.left;
     root.left = root.right;
     root.right = tmp;
-
     invertTree(root.left);
     invertTree(root.right);
-
     return root;
   }
 
