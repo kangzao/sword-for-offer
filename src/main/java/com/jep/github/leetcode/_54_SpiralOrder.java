9014d2211adb5f8f36bdef3850e3821f4e4f059f
Enping <enping.jep@alipay.com>
2021-04-26 15:42:51 +0800

    54

--- src/main/java/com/jep/github/leetcode/_54_SpiralOrder.java
+++ src/main/java/com/jep/github/leetcode/_54_SpiralOrder.java
@@ -1,14 +1,13 @@
-package com.jep.github.swordForOffer;
+package com.jep.github.leetcode;
 
 import java.util.ArrayList;
 import java.util.List;
 
 /*
  * @author: enping.jep
- * @create: 2021-04-22 7:45 下午
+ * @create: 2021-04-26 10:19 上午
  */
-public class _29_SpiralOrder {
-
+public class _54_SpiralOrder {
 
   public List<Integer> spiralOrder(int[][] matrix) {
     if (matrix == null || matrix.length == 0) {
@@ -17,38 +16,30 @@ public class _29_SpiralOrder {
 
     int down = matrix.length - 1, right = matrix[0].length - 1, left = 0, up = 0;
     List<Integer> res = new ArrayList<>((down + 1) * (right + 1));
-    while (true) {
+    while (left <= right && up <= down) {
       //left to right
-      for (int i = left; i <= right; i++) {
+      for (int i = 0; i <= right; i++) {
         res.add(matrix[up][i]);
       }
-      if (++up > down) {
-        break;
-      }
-
+      up++;
       //up to down
       for (int i = up; i <= down; i++) {
-        res.add(matrix[i][right]);
+        res.add(matrix[right][i]);
       }
-      if (--right < left) {
-        break;
-      }
+      right--;
       //right to left
       for (int i = right; i >= left; i--) {
         res.add(matrix[down][i]);
       }
-      if (--down < up) {
-        break;
-      }
+      down--;
       //down to up
       for (int i = down; i >= up; i--) {
-        res.add(matrix[i][left]);
+        res.add(matrix[down][i]);
       }
-      if (++left > right) {
-        break;
-      }
+      left++;
     }
     return res;
-  }
-}
 
+  }
+
+}
