4f3701956e7271fbf2e570798d8ed50f7cb4a82d
Enping <enping.jep@alipay.com>
2021-05-14 11:25:54 +0800

    124

--- src/main/java/com/jep/github/leetcode/_124_MaxPathSum.java
+++ src/main/java/com/jep/github/leetcode/_124_MaxPathSum.java
@@ -3,43 +3,37 @@ package com.jep.github.leetcode;
 import java.util.List;
 
 /**
- * author jiangenping
- * 2020/8/26 下午10:39
+ * author jiangenping 2020/8/26 下午10:39
  */
 
 public class _124_MaxPathSum {
 
-    private static int max = Integer.MIN_VALUE;
+  int maxValue = Integer.MIN_VALUE;
+  public int maxPathSum(TreeNode root) {
+    getOneSideMaxValue(root);
+    return maxValue;
+  }
 
-    public static int maxPathSum(TreeNode root) {
-        max = dfs(root);
-        return max;
+  //左子树或者右子树的最大值，因为从上游下来，要么走左子树,要么走右子树，然后更新到全局最大和
+  /**
+    a
+   / \
+  b  c   a是根节点，与上层父节点相连(如果有)，b、c是子节点，与其各自子节点中路径最大的节点相连
+   所有可能的情况： b+a+c  b+a c+a
+   **/
+  public int getOneSideMaxValue(TreeNode root){
+
+    if(root == null){
+      return 0;
     }
-
-    public static int dfs(TreeNode root) {
-        if (root == null) {
-            return 0;
-        }
-        int left = dfs(root.left);
-        int right = dfs(root.right);
-        int res = Math.max(root.val, Math.max(root.val + left, root.val + right));
-        max = Math.max(max, Math.max(res, root.val + left + right));
-        return res;
-    }
-
-
-    public static void main(String[] args) {
-
-        TreeNode treeNode3 = new TreeNode(15);
-        TreeNode treeNode4 = new TreeNode(7);
-        TreeNode treeNode1 = new TreeNode(9);
-        TreeNode treeNode2 = new TreeNode(20);
-        treeNode1.left = treeNode3;
-        treeNode2.right = treeNode4;
-        TreeNode root = new TreeNode(3, treeNode1, treeNode2);
-
-
-        System.out.println(dfs(root));
-
-    }
+    // 左子树提供的最大路径和
+    int leftMaxValue = Math.max(0,getOneSideMaxValue(root.left));
+    // 右子树提供的最大路径和
+    int rightMaxValue = Math.max(0,getOneSideMaxValue(root.right));
+    // 当前子树内部的最大路径和并挑战最大纪录
+    maxValue = Math.max(maxValue,leftMaxValue + root.val + rightMaxValue);
+    // 当前子树对外提供的最大路径和
+    int outputMaxSum = root.val + Math.max(leftMaxValue,rightMaxValue);
+    return Math.max(outputMaxSum, 0);
+  }
 }
