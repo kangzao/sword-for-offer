c322a898d8f59ab7666647453ed68825919ca50b
Enping <enping.jep@alipay.com>
2021-05-14 15:49:32 +0800

    09

--- src/main/java/com/jep/github/swordForOffer/_09_QueueWithTwoStacks.java
+++ src/main/java/com/jep/github/swordForOffer/_09_QueueWithTwoStacks.java
@@ -11,10 +11,21 @@ import java.util.Stack;
 //用两个栈实现队列
 // 题目：用两个栈实现一个队列。队列的声明如下，请实现它的两个函数appendTail
 // 和deleteHead，分别完成在队列尾部插入结点和在队列头部删除结点的功能。
+
+/**
+ * 示例 1：
+ *
+ * 输入： ["CQueue","appendTail","deleteHead","deleteHead"] [[],[3],[],[]] 输出：[null,null,3,-1]
+ *
+ * 示例 2：
+ *
+ * 输入： ["CQueue","deleteHead","appendTail","appendTail","deleteHead","deleteHead"]
+ * [[],[],[5],[2],[],[]] 输出：[null,-1,null,null,5,2]
+ */
 public class _09_QueueWithTwoStacks {
 
-  private Stack<Integer> stack1 = new Stack<Integer>();
-  private Stack<Integer> stack2 = new Stack<Integer>();
+  private Stack<Integer> stack1 = new Stack<>();
+  private Stack<Integer> stack2 = new Stack<>();
 
   public void push(int node) {
     stack1.push(node);
@@ -32,7 +43,7 @@ public class _09_QueueWithTwoStacks {
     return stack2.pop();
   }
 
-  public static void main(String args[]) {
+  public static void main(String[] args) {
     _09_QueueWithTwoStacks queueWithTwoStacks = new _09_QueueWithTwoStacks();
     queueWithTwoStacks.push(1);
     queueWithTwoStacks.push(2);
