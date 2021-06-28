f6bedb38da2ef6b3b764d1673f93b7cace05b996
enping <58761173@qq.com>
2021-06-28 21:56:50 +0800

    680

--- src/main/java/com/jep/github/leetcode/_680_ValidPalindrome.java
+++ src/main/java/com/jep/github/leetcode/_680_ValidPalindrome.java
@@ -6,29 +6,55 @@ package com.jep.github.leetcode;
  */
 public class _680_ValidPalindrome {
 
-  public static boolean validPalindrome(String s) {
-    for (int i = 0, j = s.length() - 1; i < j; i++, j--) {
-      if (s.charAt(i) != s.charAt(j)) {
-        return isPalindrome(s, i + 1, j) || isPalindrome(s, i, j - 1);
-      }
+    public boolean validPalindrome(String s) {
+        int head = 0, tail = s.length() - 1;
+        while (head < tail) {
+            if (s.charAt(head) == s.charAt(tail)) {
+                head++;
+                tail--;
+            } else {
+                return isPalindrome(s, head + 1, tail) || isPalindrome(s, head, tail - 1);
+            }
+        }
+        return true;
+
+
     }
-    return true;
 
-  }
-
-  public static boolean isPalindrome(String s, int i, int j) {
-
-    while (i < j) {
-      if (s.charAt(i++) != s.charAt(j--)) {
-        return false;
-      }
+    public boolean isPalindrome(String s, int head, int tail) {
+        while (head < tail) {
+            if (s.charAt(head++) != s.charAt(tail--)) {
+                return false;
+            }
+        }
+        return true;
     }
-    return true;
 
-  }
+//  public static boolean validPalindrome(String s) {
+//    for (int i = 0, j = s.length() - 1; i < j; i++, j--) {
+//      if (s.charAt(i) != s.charAt(j)) {
+//        return isPalindrome(s, i + 1, j) || isPalindrome(s, i, j - 1);
+//      }
+//    }
+//    return true;
+//
+//  }
+//
+//  public static boolean isPalindrome(String s, int i, int j) {
+//
+//    while (i < j) {
+//      if (s.charAt(i++) != s.charAt(j--)) {
+//        return false;
+//      }
+//    }
+//    return true;
+//
+//  }
 
-  public static void main(String args[]) {
-    System.out.println(validPalindrome("abca"));
-  }
+    public static void main(String args[]) {
+//        System.out.println(validPalindrome("abca"));
+        _680_ValidPalindrome validPalindrome = new _680_ValidPalindrome();
+        validPalindrome.validPalindrome("abc");
+    }
 
 }
