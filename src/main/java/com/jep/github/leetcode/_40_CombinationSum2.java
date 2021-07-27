6a9a6734eee9a84fd3b9cb98b71f281083c32392
Enping <enping.jep@alipay.com>
2021-07-27 20:45:23 +0800

    40

--- src/main/java/com/jep/github/leetcode/_40_CombinationSum2.java
+++ src/main/java/com/jep/github/leetcode/_40_CombinationSum2.java
@@ -20,30 +20,30 @@ public class _40_CombinationSum2 {
   public List<List<Integer>> combinationSum2(int[] candidates, int target) {
     Arrays.sort(candidates);//将重复元素放在一起
     List<Integer> path = new ArrayList<>();
-    dfs(candidates, path, target, 0, 0);
+    dfs(candidates, path, target, 0);
     return res;
   }
 
-  public void dfs(int[] candidates, List<Integer> path, int target, int sum, int begin) {
-    if (sum == target) {
+  public void dfs(int[] candidates, List<Integer> path, int remain, int begin) {
+    if (remain == 0) {
       res.add(new ArrayList<>(path));
       return;
     }
     //循环选定一个元素
     for (int i = begin; i < candidates.length; i++) {
-      if (candidates[i] > target) {
+      if (candidates[i] > remain) {
         return;
       }
       //除重
       if (i > begin && candidates[i] == candidates[i - 1]) {
         continue;
       }
-      int n = sum + candidates[i];
+
       //必须是小于等于 因为单个元素可能满足 candidates[i] == target
-      if (n <= target) {
+      if (remain >= 0) {
         path.add(candidates[i]);
         //递归选择后续的元素
-        dfs(candidates, path, target, n, i + 1);
+        dfs(candidates, path, remain - candidates[i], i + 1);
         path.remove(path.size() - 1);
       }
     }
