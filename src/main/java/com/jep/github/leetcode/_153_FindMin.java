e6fc88f6cee4103b72caea9f01f57c8fecf07651
Enping <enping.jep@alipay.com>
2021-07-28 15:42:59 +0800

    153

--- src/main/java/com/jep/github/leetcode/_153_FindMin.java
+++ src/main/java/com/jep/github/leetcode/_153_FindMin.java
@@ -1,30 +1,30 @@
 package com.jep.github.leetcode;
 
 /**
- * author jiangenping
- * 2020/8/2 下午1:17
+ * author jiangenping 2020/8/2 下午1:17
  */
 
 public class _153_FindMin {
 
-    public static int findMin(int[] nums) {
-        int left = 0, right = nums.length - 1;
-        while (left < right) {
-            if (nums[left] < nums[right])
-                return nums[left];
-            int mid = left + (right - left) / 2;
-            if (nums[mid] >= nums[left]) {
-                left = mid + 1;
-            } else if (nums[right] > nums[mid]) {
-                right = mid;
-            }
-        }
+  public static int findMin(int[] nums) {
+    int left = 0, right = nums.length - 1;
+    while (left < right) {
+      if (nums[left] < nums[right]) {
         return nums[left];
+      }
+      int mid = left + (right - left) / 2;
+      if (nums[mid] >= nums[left]) {
+        left = mid + 1;
+      } else if (nums[right] > nums[mid]) {
+        right = mid;
+      }
     }
+    return nums[left];
+  }
 
-    public static void main(String[] args) {
-        int[] array = {4, 5, 6, 7, 0, 1, 2};  // 4,5,6,7,0,1,2
-        System.out.println(findMin(array));
+  public static void main(String[] args) {
+    int[] array = {4, 5, 6, 7, 0, 1, 2};  // 4,5,6,7,0,1,2
+    System.out.println(findMin(array));
 
-    }
+  }
 }
