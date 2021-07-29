f8b91dc2d4251733099d1b978d4f0861b13a2de0
enping <58761173@qq.com>
2021-07-29 21:34:24 +0800

    138

---
+++ src/main/java/com/jep/github/leetcode/_138_CopyRandomList.java
@@ -0,0 +1,27 @@
+package com.jep.github.leetcode;
+
+import java.util.Map;
+
+/**
+ * @author enping
+ * @date 2021/7/29 9:33 下午
+ **/
+public class _138_CopyRandomList {
+//    public Node copyRandomList(Node head) {
+//        Map<Node, Node> map = new HashMap<>();
+//        Node dummy = head;
+//        while (head != null) {
+//            Node node = new Node(head.val);
+//            map.put(head, node);
+//            head = head.next;
+//        }
+//        head = dummy;
+//        while (head != null) {
+//            Node node = map.get(head);
+//            node.next = map.get(head.next);
+//            node.random = map.get(head.random);
+//            head = head.next;
+//        }
+//        return map.get(dummy);
+//    }
+}
