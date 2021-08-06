373ac368165c34cd9dc46d56d7440cd774e8ed53
Enping <enping.jep@alipay.com>
2021-08-06 17:35:27 +0800

    91

--- src/main/java/com/jep/github/leetcode/_91_NumDecodings.java
+++ src/main/java/com/jep/github/leetcode/_91_NumDecodings.java
@@ -8,28 +8,28 @@ public class _91_NumDecodings {
 
 
   public int numDecodings(String s) {
-    if (s == null || s.length() == 0 || s.charAt(0) == '0') {
-      return 0;
-    }
     char[] array = s.toCharArray();
-    //定义dp[i]为前i个数字的解码数量
-    int[] dp = new int[s.length() + 1];
-    //初始化
-    dp[1] = dp[0] = 1;
-    //第i-1个当做一个字符，则dp[i] = dp[i-1]
-    for (int i = 2; i <= s.length(); i++) {
-      if (array[i - 1] > '0' && array[i - 1] <= '9') {
+    int n = array.length;
+    int[] dp = new int[n + 1];//前n个字符的解码方式
+    dp[0] = 1;//前0个字符有一种解码方式
+
+    for (int i = 1; i <= n; i++) {
+      //第i位单独解码
+      if (array[i - 1] != '0') {
         dp[i] = dp[i - 1];
       }
-
-      if (array[i - 2] != '0' && (array[i - 2] - '0') * 10 + (array[i - 1] - '0') <= 26) {
-        dp[i] += dp[i - 2];
+      //第i-1位和第i位一起解码
+      if (i > 1) {
+        //注意，这里必须将char字符进行偏移量的计算，不然 int res = ('1' - '0') * 10 + '2'   res是60
+        int item = (array[i - 2] - '0') * 10 + array[i - 1] - '0';
+        if (item >= 10 && item <= 26) {
+          dp[i] += dp[i - 2];
+        }
       }
-
     }
-    return dp[s.length()];
-
+    return dp[n];
   }
+
 
   public static void main(String args[]) {
     _91_NumDecodings solution = new _91_NumDecodings();
