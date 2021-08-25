31a86f57df7d372542075394d5d13bb401a3e09d
Enping <enping.jep@alipay.com>
2021-08-25 14:56:57 +0800

    45

--- src/main/java/com/jep/github/leetcode/_45_Jump.java
+++ src/main/java/com/jep/github/leetcode/_45_Jump.java
@@ -14,6 +14,7 @@ public class _45_Jump {
     for (int i = 0; i < nums.length - 1; i++) {
       maxPositon = Math.max(maxPositon, i + nums[i]);
       if (end == i) {
+        //选择落点
         end = maxPositon;
         step++;
       }
