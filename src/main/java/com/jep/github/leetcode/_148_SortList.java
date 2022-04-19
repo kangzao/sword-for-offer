e8cd62f4cf6ee0954c0f0e6895d83a51d747460b
Enping <enping.jep@alipay.com>
2022-04-19 18:50:13 +0800

    148

--- src/main/java/com/jep/github/leetcode/_148_SortList.java
+++ src/main/java/com/jep/github/leetcode/_148_SortList.java
@@ -41,4 +41,70 @@ public class _148_SortList {
       return right;
     }
   }
+
+  public ListNode sortList_1(ListNode head) {
+    if (head == null || head.next == null) {
+      return head;
+    }
+    //找到中点
+    System.out.println("head param ==" + head.val);
+    ListNode mid = middleNode(head);
+    System.out.println("mid ==" + mid.val);
+    ListNode rightHead = mid.next;
+    //断开链表
+    mid.next = null;
+    //递归
+    ListNode left = sortList_1(head);//将左半部分排序
+    ListNode right = sortList_1(rightHead);//将右半部分排序
+    //合并
+    return mergeNode(left, right);
+  }
+
+  private ListNode middleNode(ListNode head) {
+
+    if (head == null || head.next == null) {
+      return head;
+    }
+    ListNode fast = head, slow = head;
+    while (fast != null && fast.next != null) {
+      fast = fast.next.next;
+      slow = slow.next;
+    }
+    return slow;
+  }
+
+  private ListNode mergeNode(ListNode p, ListNode q) {
+    ListNode pre = new ListNode(-1), dummy = pre;
+    while (p != null && q != null) {
+      if (p.val < q.val) {
+        pre.next = p;
+        p = p.next;
+      } else {
+        pre.next = q;
+        q = q.next;
+      }
+      pre = pre.next;
+    }
+
+    pre.next = p == null ? q : p;
+
+    return dummy.next;
+
+  }
+
+
+  public static void main(String args[]) {
+    ListNode l1 = new ListNode(4);
+    ListNode l2 = new ListNode(2);
+    ListNode l3 = new ListNode(1);
+    ListNode l4 = new ListNode(3);
+    l1.next = l2;
+    l2.next = l3;
+    l3.next = l4;
+    l1.printNode();
+    _148_SortList sortList = new _148_SortList();
+    ListNode node = sortList.sortList_1(l1);
+    node.printNode();
+
+  }
 }
