2cf30ab6180487de836ef1a1f2061bc3ad0b0ab3
enping <58761173@qq.com>
2022-05-07 19:13:51 +0800

    199

--- src/main/java/com/jep/github/leetcode/_199_RightSideView.java
+++ src/main/java/com/jep/github/leetcode/_199_RightSideView.java
@@ -1,9 +1,6 @@
 package com.jep.github.leetcode;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
+import java.util.*;
 
 /*
  * @author: enping.jep
@@ -11,30 +8,30 @@ import java.util.Queue;
  */
 public class _199_RightSideView {
 
-  public List<Integer> rightSideView(TreeNode root) {
-    List<Integer> list = new ArrayList();
-    if (root == null) {
-      return list;
-    }
-    Queue<TreeNode> queue = new LinkedList();
-    queue.offer(root);
-    while (!queue.isEmpty()) {
-      int size = queue.size();
-      for (int i = 0; i < size; i++) {
-        TreeNode node = queue.poll();
-        if (node.left != null) {
-          queue.offer(node.left);
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        if (root == null) {
+            return res;
         }
-        if (node.right != null) {
-          queue.offer(node.right);
-        }
-        if (i == size - 1) {
-          list.add(node.val);
-        }
+        Deque<TreeNode> deque = new ArrayDeque<>();
+        deque.offer(root);
+        while (!deque.isEmpty()) {
 
-      }
+            //遍历每一层
+            for (int i = deque.size() - 1; i >= 0; i--) {
+                TreeNode node = deque.poll();
+                if (node.left != null) {
+                    deque.offer(node.left);
+                }
+                if (node.right != null) {
+                    deque.offer(node.right);
+                }
+                if (i == 0) {
+                    res.add(node.val);
+                }
+            }
+        }
+        return res;
     }
-    return list;
-  }
 
 }
