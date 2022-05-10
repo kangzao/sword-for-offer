e5c0a63002a7b31a1a431349a89ca753f13cb431
Enping <enping.jep@alipay.com>
2022-05-10 21:03:27 +0800

    98

--- src/main/java/com/jep/github/leetcode/_98_IsValidBST.java
+++ src/main/java/com/jep/github/leetcode/_98_IsValidBST.java
@@ -6,55 +6,33 @@ package com.jep.github.leetcode;
  */
 public class _98_IsValidBST {
 
-    long pre = Long.MIN_VALUE;
+  TreeNode pre;
 
-    public boolean isValidBST_short(TreeNode root) {
-        if (root == null) {
-            return true;
-        }
-        if (!isValidBST_short(root.left)) {
-            return false;
-        }
-        //中序遍历一定是升序，所以如果当前节点小于等于前一个节点，则不是二叉搜索树
-        if (root.val <= pre) {
-            return false;
-        }
-        pre = root.val;
-        return isValidBST_short(root.right);
+  public boolean isValidBST(TreeNode root) {
+    if (root == null) {
+      return true;
+    }
+    if (!isValidBST(root.left)) {
+      return false;
     }
 
-
-    public boolean isValidBST(TreeNode root) {
-
-        return isValidBST(root, null, null);
+    //中序遍历必须递增
+    if (pre != null && root.val <= pre.val) {
+      return false;
     }
+    pre = root;
+    return isValidBST(root.right);
+  }
 
-    //  函数表示考虑以 root 为根的子树，判断子树中所有节点的值是否都在 (l,r)的范围内（注意是开区间）
-    //  如果 root 节点的值 val 不在 (l,r)(l,r) 的范围内说明不满足条件直接返回，否则我们要继续递归调用检查它的左右子树是否满足，
-    //  如果都满足才说明这是一棵二叉搜索树。
+  public static void main(String args[]) {
+    TreeNode root = new TreeNode(5);
+    root.left = new TreeNode(1);
+    root.right = new TreeNode(4);
+    root.right.left = new TreeNode(3);
+    root.right.right = new TreeNode(6);
+    _98_IsValidBST isValidBSTInstance = new _98_IsValidBST();
+    isValidBSTInstance.isValidBST(root);
 
-    public boolean isValidBST(TreeNode root, TreeNode min, TreeNode max) {
-        if (root == null) {
-            return true;
-        }
-        if (min != null && root.val <= min.val) {
-            return false;
-        }
-        if (max != null && root.val >= max.val) {
-            return false;
-        }
-        return isValidBST(root.left, null, root) && isValidBST(root.right, root, null);
-    }
-
-    public static void main(String args[]) {
-        TreeNode root = new TreeNode(5);
-        root.left = new TreeNode(1);
-        root.right = new TreeNode(4);
-        root.right.left = new TreeNode(3);
-        root.right.right = new TreeNode(6);
-        _98_IsValidBST isValidBSTInstance = new _98_IsValidBST();
-        isValidBSTInstance.isValidBST(root, null, null);
-
-    }
+  }
 
 }
