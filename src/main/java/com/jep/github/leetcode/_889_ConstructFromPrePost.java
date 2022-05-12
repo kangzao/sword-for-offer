ed18a3c02c4ccc2823afd50e6197834f243cebd2
Enping <enping.jep@alipay.com>
2022-05-12 21:13:16 +0800

    889

---
+++ src/main/java/com/jep/github/leetcode/_889_ConstructFromPrePost.java
@@ -0,0 +1,48 @@
+package com.jep.github.leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/*
+ * @author: enping.jep
+ * @create: 2022-05-12 9:12 下午
+ */
+public class _889_ConstructFromPrePost {
+
+  Map<Integer, Integer> map = new HashMap<>();
+
+  public TreeNode constructFromPrePost(int[] preorder, int[] postorder) {
+    //pre:1 245 367  post:452  673  1
+    for (int i = 0; i < postorder.length; i++) {
+      map.put(postorder[i], i);
+    }
+    return build(preorder, 0, preorder.length - 1, postorder, 0, postorder.length - 1);
+  }
+
+  public TreeNode build(int[] preorder, int preStart, int preEnd, int[] postorder, int postStart,
+      int postEnd) {
+
+    if (preStart > preEnd) {
+      return null;
+    }
+
+    if (preStart == preEnd) {
+      return new TreeNode(preorder[preStart]);
+    }
+    //初始化根节点
+    int rootVal = preorder[preStart];
+
+    int leftRootVal = preorder[preStart + 1];//左子树的值
+
+    //根据前序第二个元素确定左子树长度：该长度为该元素在postorder中的位置 + 1
+    int index = map.get(leftRootVal);
+    int leftLen = index - postStart + 1;
+
+    TreeNode root = new TreeNode(rootVal);
+    root.left = build(preorder, preStart + 1, preStart + leftLen, postorder, postStart, index);
+    root.right = build(preorder, preStart + leftLen + 1, preEnd, postorder, index + 1, postEnd - 1);
+    return root;
+
+  }
+
+}
