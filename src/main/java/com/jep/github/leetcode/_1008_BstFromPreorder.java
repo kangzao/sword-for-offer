0a334d5acc35a6217899e8be1f90edf8ca2e060c
Enping <enping.jep@alipay.com>
2022-05-26 17:18:18 +0800

    1008

---
+++ src/main/java/com/jep/github/leetcode/_1008_BstFromPreorder.java
@@ -0,0 +1,37 @@
+package com.jep.github.leetcode;
+
+import apple.laf.JRSUIUtils.Tree;
+
+/*
+ * @author: enping.jep
+ * @create: 2022-05-26 4:22 下午
+ * 给一个二叉搜索树前序遍历的数组，还原这颗二叉搜索树
+ */
+public class _1008_BstFromPreorder {
+
+  //preorder = [8, 5,1,7, 10,12]
+  public TreeNode bstFromPreorder(int[] preorder) {
+    return build(preorder, 0, preorder.length - 1);
+  }
+
+  public TreeNode build(int[] preorder, int start, int end) {
+    if (start > end) {
+      return null;
+    }
+    TreeNode root = new TreeNode(preorder[start]);
+    if (start == end) {
+      return root;
+    }
+    //确定左子树范围,二叉搜索树,左子树的值均小于根节点，右子树的值均大于根节点
+    //防止两个节点的情况，所以i = start，而不是i = start + 1
+    int i = start;
+    while (i + 1 <= end && preorder[i + 1] < preorder[start]) {
+      i++;
+    }
+    //preorder = [8, 5,1,7, 10,12]
+    root.left = build(preorder, start + 1, i);
+    root.right = build(preorder, i + 1, end);
+    return root;
+  }
+
+}
