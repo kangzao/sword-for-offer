af3615acc97083a442fad29862cac6543a698daa
Enping <enping.jep@alipay.com>
2022-06-01 15:22:38 +0800

    95

--- src/main/java/com/jep/github/leetcode/_95_GenerateTrees.java
+++ src/main/java/com/jep/github/leetcode/_95_GenerateTrees.java
@@ -1,5 +1,6 @@
 package com.jep.github.leetcode;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /*
@@ -10,7 +11,35 @@ import java.util.List;
 public class _95_GenerateTrees {
 
   public List<TreeNode> generateTrees(int n) {
+    if (n < 1) {
+      return new ArrayList<>();
+    }
+    return build(1, n);
+  }
 
+  //递归构造树
+  public List<TreeNode> build(int start, int end) {
+    List<TreeNode> list = new ArrayList<>();
+
+    if (start > end) {
+      //start大于end时，说明是空节点，空节点也要加入到列表中，以避免下方嵌套for循环进不去
+      list.add(null);
+      return list;
+    }
+    for (int i = start; i <= end; i++) {
+      List<TreeNode> leftTree = build(start, i - 1);
+      List<TreeNode> rightTree = build(i + 1, end);
+      //左子树不动，构建右子树
+      for (TreeNode left : leftTree) {
+        for (TreeNode right : rightTree) {
+          TreeNode root = new TreeNode(i);
+          root.left = left;
+          root.right = right;
+          list.add(root);
+        }
+      }
+    }
+    return list;
   }
 
 }
