7fe932131d59b9656d6cd1a66681b12b166fa512
Enping <enping.jep@alipay.com>
2022-06-08 16:20:24 +0800

    124

--- src/main/java/com/jep/github/leetcode/_124_MaxPathSum.java
+++ src/main/java/com/jep/github/leetcode/_124_MaxPathSum.java
@@ -3,30 +3,30 @@ package com.jep.github.leetcode;
 import java.util.List;
 
 /**
- * author jiangenping 2020/8/26 下午10:39
+ * author jiangenping 2020/8/26 下午10:39 update 2022年06月08日16:19:39
  */
 
 public class _124_MaxPathSum {
 
-    int maxVal = Integer.MIN_VALUE;
+  private int maxSum = Integer.MIN_VALUE;
 
-    public int maxPathSum(TreeNode root) {
-        maxGain(root);
-        return maxVal;
+  public int maxPathSum(TreeNode root) {
+    maxGain(root);
+    return maxSum;
+  }
 
+  //当前节点要么和左右子数的最大收益组成最大路径和，要么左右选一个最大的然后向上找父节点(防止走回头路)
+  //计算当前节点为父节点提供的最大收益
+  public int maxGain(TreeNode root) {
+    if (root == null) {
+      return 0;
     }
-
-    //从root出发的子树的最大收益
-    public int maxGain(TreeNode root) {
-        if (root == null) {
-            return 0;
-        }
-        //最大收益必须大于0才有意义，0以下的数字都返回0，即：舍弃该节点
-        int leftGain = Math.max(maxGain(root.left), 0);
-        int rightGain = Math.max(maxGain(root.right), 0);
-        maxVal = Math.max(maxVal, root.val + leftGain + rightGain);
-        //只能返回左子树和右子树的最大值，只能选择一条边，两条边同时选意味着root被经过了两次，不符合提议
-        return root.val + Math.max(leftGain, rightGain);
-
-    }
+    int left = maxGain(root.left);
+    int right = maxGain(root.right);
+    //经过当前节点的最大路径和
+    maxSum = Math.max(left + right + root.val, maxSum);
+    //计算当前对外的最大贡献值(要么选左子树，要么选右子树)
+    int sum = Math.max(left + root.val, right + root.val);
+    return Math.max(sum, 0);
+  }
 }
