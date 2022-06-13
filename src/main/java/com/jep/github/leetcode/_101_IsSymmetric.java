0b9830b27aecbd35cd574d507c075a1a7eb27ee2
Enping <enping.jep@alipay.com>
2022-06-13 14:59:36 +0800

    101

--- src/main/java/com/jep/github/leetcode/_101_IsSymmetric.java
+++ src/main/java/com/jep/github/leetcode/_101_IsSymmetric.java
@@ -3,6 +3,7 @@ package com.jep.github.leetcode;
 /*
  * @author: enping.jep
  * @create: 2021-04-08 4:41 下午
+ * 2022年06月13日14:59:26
  */
 public class _101_IsSymmetric {
 
@@ -10,20 +11,21 @@ public class _101_IsSymmetric {
     if (root == null) {
       return true;
     }
-    TreeNode left = root.left;
-    TreeNode right = root.right;
-    return compare(left, right);
+    return isSymmetric(root.left, root.right);
   }
 
-  public boolean compare(TreeNode left, TreeNode right) {
-    if (left == null || right == null) {
-      return right == left;
+  public boolean isSymmetric(TreeNode left, TreeNode right) {
+    if (left == null && right == null) {
+      return true;
     }
-
+    if (left == null || right == null) {
+      return false;
+    }
     if (left.val != right.val) {
       return false;
     }
-    return compare(left.left, right.right) && compare(left.right, right.left);
+    return isSymmetric(left.left, right.right) && isSymmetric(left.right, right.left);
+
   }
 
 }
