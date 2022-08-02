8488dad665719d4aaf5e5c1986ff04a7eede3569
Enping <enping.jep@alipay.com>
2022-08-02 16:52:04 +0800

    64

--- src/main/java/com/jep/github/leetcode/_64_MinPathSum.java
+++ src/main/java/com/jep/github/leetcode/_64_MinPathSum.java
@@ -14,70 +14,75 @@ import java.util.Arrays;
  */
 public class _64_MinPathSum {
 
-    public int minPathSum(int[][] grid) {
+  public int minPathSum(int[][] grid) {
+    /**
+     131
+     151
+     421
+     */
+    int m = grid.length;
+    int n = grid[0].length;
+    int[][] dp = new int[m][n];//dp[i][j]表示从左上角移动到grid[i][j]的最短路径
+    dp[0][0] = grid[0][0];
+    //第一列 只能向下
+    for (int i = 1; i < m; i++) {
+      dp[i][0] = dp[i - 1][0] + grid[i][0];
+    }
+    //第一行 只能向右
+    for (int i = 1; i < n; i++) {
+      dp[0][i] = dp[0][i - 1] + grid[0][i];
+    }
+    for (int i = 1; i < m; i++) {
+      for (int j = 1; j < n; j++) {
+        //只能向下或者向右
+        dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
+      }
+    }
+    return dp[m - 1][n - 1];
+  }
 
-        int row = grid.length;
-        int col = grid[0].length;
-
-        int[][] dp = new int[row][col];
-        dp[0][0] = grid[0][0];
-        for (int i = 1; i < row; i++) {
-            dp[i][0] = dp[i - 1][0] + grid[i][0];
-        }
-        for (int i = 1; i < col; i++) {
-            dp[0][i] = dp[0][i - 1] + grid[0][i];
-        }
-        for (int i = 1; i < row; i++) {
-            for (int j = 1; j < col; j++) {
-                dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
-            }
-        }
-        return dp[row - 1][col - 1];
-
+  public int minPathSum_rollArray(int[][] grid) {
+    int m = grid.length;
+    if (m == 0) {
+      return 0;
+    }
+    int n = grid[0].length;
+    if (n == 0) {
+      return 0;
     }
 
-    public int minPathSum_rollArray(int[][] grid) {
-        int m = grid.length;
-        if (m == 0) {
-            return 0;
-        }
-        int n = grid[0].length;
-        if (n == 0) {
-            return 0;
-        }
-
-        int[][] dp = new int[2][n];
-        int old, now = 0;
+    int[][] dp = new int[2][n];
+    int old, now = 0;
 //        dp[i][j] = min{dp[i-1][j],dp[i][j-1]}+grid[i][j]
-        for (int i = 0; i < m; i++) {
-            old = now;
-            now = 1 - now;
-            for (int j = 0; j < n; j++) {
-                if (i == 0 && j == 0) {
-                    dp[now][j] = grid[i][j];
-                    continue;
-                }
-                int tmp = Integer.MAX_VALUE;
-                if (i != 0) {
-                    tmp = Math.min(tmp, dp[old][j]);
-                }
-
-                if (j != 0) {
-                    tmp = Math.min(tmp, dp[now][j - 1]);
-                }
-                dp[now][j] = tmp + grid[i][j];
-            }
+    for (int i = 0; i < m; i++) {
+      old = now;
+      now = 1 - now;
+      for (int j = 0; j < n; j++) {
+        if (i == 0 && j == 0) {
+          dp[now][j] = grid[i][j];
+          continue;
         }
-        return dp[now][n - 1];
+        int tmp = Integer.MAX_VALUE;
+        if (i != 0) {
+          tmp = Math.min(tmp, dp[old][j]);
+        }
 
+        if (j != 0) {
+          tmp = Math.min(tmp, dp[now][j - 1]);
+        }
+        dp[now][j] = tmp + grid[i][j];
+      }
     }
+    return dp[now][n - 1];
+
+  }
 
 
-    public static void main(String args[]) {
-        _64_MinPathSum solution = new _64_MinPathSum();
-        int[][] array = {{1, 3, 1}, {1, 5, 1}, {4, 2, 1}};
-        System.out.println(solution.minPathSum_rollArray(array));
-    }
+  public static void main(String args[]) {
+    _64_MinPathSum solution = new _64_MinPathSum();
+    int[][] array = {{1, 3, 1}, {1, 5, 1}, {4, 2, 1}};
+    System.out.println(solution.minPathSum_rollArray(array));
+  }
 
 
 }
